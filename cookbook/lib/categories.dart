875f48ef008992ee2461e4e984cb67a12a9f2668
Rithik1611 <rithik161104@gmail.com>
2024-07-12 10:03:01 +0530

    Screen one

---
+++ cookbook/lib/categories.dart
@@ -0,0 +1,29 @@
+import 'package:cookbook/category_grid_item.dart';
+import 'package:cookbook/dummy_data.dart';
+import 'package:flutter/material.dart';
+
+class Categories extends StatelessWidget {
+  const Categories({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Select a Category'),
+        centerTitle: false,
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(10),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+        ),
+        children: [
+          for (final i in availableCategories) CategoryGridItem(category: i)
+        ],
+      ),
+    );
+  }
+}
